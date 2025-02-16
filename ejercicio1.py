bacd442910c134829eb1415ee364880da2ac4f01
Mateolr28 <mateolrdev@outlook.com>
2025-02-16 13:50:14 -0500

    Ejercicios

---
+++ ejercicio1.py
@@ -0,0 +1,11 @@
+'''A un grupo de hombres y mujeres les realizan una prueba de conocimientos. Se 
+desea conocer cuántas mujeres y cuántos hombres presentaron la prueba y el total 
+de personas que se procesaron. '''
+
+
+hombres = int(input("Ingrese la cantidad de hombres: "))
+mujeres = int(input("Ingrese la cantidad de mujeres: "))
+
+total = hombres + mujeres
+
+print(f"Total de hombres: {hombres}, total de mujeres: {mujeres}, total de personas: {total}")
